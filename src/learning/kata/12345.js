815e7175f52e043c278952e02f068a5c10c215b5
LyskoDS <lysko_dima@mail.ru>
2023-01-26 22:01:20 +0300

    add new

---
+++ src/learning/kata/12345.js
@@ -0,0 +1,13 @@
+function _if(bool, func1, func2) {
+    function a(func1) {
+    const b = "True";
+    return b;
+    }
+}
+
+    if (bool = "True") {
+        return func1;
+    } else {
+        return func2;
+    }
+    return 0;
